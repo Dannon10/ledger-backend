44036cdd3d6a7b209906d25806febbdb8501ba0f
Dannon Abayomi <abayomidannon10@gmail.com>
2026-09-24 17:37:33 +0100

    chore: updated PORT

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv  from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
 import clientRoutes from './routes/clientRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 import invoiceRoutes from './routes/invoiceRoutes.js';
@@ -11,21 +12,28 @@ import dashboardRoutes from './routes/dashboardRoutes.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/invoices', invoiceRoutes);
 app.use('/api/dashboard', dashboardRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected sucessfully'))
-.catch((err) =>  console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected sucessfully'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/ping', (req, res) => {
-    res.json({message: 'pong'});
+    res.json({ message: 'pong' });
 });
 
 app.listen(PORT, () => {
